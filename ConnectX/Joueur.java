67238de9805634b88176424121385eb48e736dcd
conorevans98 <43791257+conorevans98@users.noreply.github.com>
2018-12-18 17:59:43 +0100

    Update and rename Joueur to Joueur.java

---
+++ ConnectX/Joueur.java
@@ -0,0 +1,12 @@
+/**
+ * @author Conor Evans
+ * @date 2018-12-18
+ */
+
+/**
+ * Abstract class 'Player' which can be extended to realise a Human player or a
+ * Random player
+ */
+public abstract class Joueur {
+	public abstract int choixColonne(JeuPuissanceZ grille);
+}
